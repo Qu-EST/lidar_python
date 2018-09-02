56211cd934c5d11f28d81d0833963ed6c11ba070
jeeva <jee1199@gmail.com>
2018-09-02 15:49:43 +0000

    minor bugs in lidar.py

--- lidar.py
+++ lidar.py
@@ -6,9 +6,9 @@ import counter
 parser = argparse.ArgumentParser()
 parser.add_argument("-x","--xmin", type=float, help="X min value", default=0.5)
 parser.add_argument("-X", "--xmax", type=float, help="X max value", default =1)
-parser.add_argument("-y", "--ymin", type=float, help="Y min value", defalut=0.5)
+parser.add_argument("-y", "--ymin", type=float, help="Y min value", default=0.5)
 parser.add_argument("-Y", "--ymax", type=float, help="Y max value", default=1)
-parser.add_argument("-z", "--zmin", type=float, help="Z min value", defalut=0)
+parser.add_argument("-z", "--zmin", type=float, help="Z min value", default=0)
 parser.add_argument("-Z", "--zmax", type=float, help="Z max value", default=200)
 parser.add_argument('-s', '--xstep', type=float, help='x step size', default=0.1)
 parser.add_argument('-u', '--ystep', type=float, help='y stepsize', default=0.1)
@@ -21,7 +21,7 @@ parser.add_argument('-l', '--lidar', help='doing a normal lidar', action='store_
 parser.add_argument('-v', '--verbose', help='print the verbose', action='store_true')
 
 
-arg = parser.parse_args()
+args = parser.parse_args()
 
 delay_fd = Gpib(name=0, pad=5)
 if(delay_fd<0):
