5364f2474a5821161b8513905a00a7c36d9187b3
jeeva <jee1199@gmail.com>
2018-09-12 15:07:30 +0000

    fixed the recursion issue, and some changes in lidar

--- test_counter.py
+++ test_counter.py
@@ -1,8 +1,8 @@
 import counter as c
 import time
 
-for x in range(0,10000):
-    count_fd, control_fd = c.open_counter(10)
-    print c.count(control_fd, count_fd, 10)
-    time.sleep(10.0/1000)
-    c.close(control_fd, count_fd)
+#for x in range(0,10000):
+count_fd, control_fd = c.open_counter(10)
+print c.count(control_fd, count_fd, 10)
+time.sleep(10.0/1000)
+c.close(control_fd, count_fd)
